bf4be2922a06d4c607eb8660464cc1a6ad011066
Akhaylin Rajagopaul <akhaylin.r@gmail.com>
2023-09-20 18:12:47 -0400

    completed step 4

--- tvmaze.js
+++ tvmaze.js
@@ -4,23 +4,7 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary:
-  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-  //          women with extraordinary skills that helped to end World War II.</p>
-  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-  //          normal lives, modestly setting aside the part they played in
-  //          producing crucial intelligence, which helped the Allies to victory
-  //          and shortened the war. When Susan discovers a hidden code behind an
-  //          unsolved murder she is met by skepticism from the police. She
-  //          quickly realises she can only begin to crack the murders and bring
-  //          the culprit to justice with her former friends.</p>`,
-  //     image:
-  //       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ];
+
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -30,14 +14,19 @@ const $searchForm = $("#searchForm");
  */
 
 async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+  let showArray = [];
+
   const params = new URLSearchParams({q:term });
   const response = await fetch(`http://api.tvmaze.com/search/shows?${params}`)
   const showData = await response.json();
 
-  let showArray = [];
-  for(let show of showData){
-    const {id, name, summary, image} = show.show //Ask about why image.original does not work in destructuring
+  for(let item of showData){
+    let {id, name, summary, image} = item.show //Ask about why image.original does not work in destructuring
+    if (image === null) {
+      image = {
+        original: 'https://tinyurl.com/tv-missing'
+      };
+    }
     showArray.push({id, name, summary, image})
   }
   return showArray;
@@ -53,15 +42,8 @@ function displayShows(shows) {
   $showsList.empty();
 
   for (const show of shows) {
-    let srcImage = show.image;
-    const defaultImage = 'https://tinyurl.com/tv-missing';
-    if(srcImage === null){
-      srcImage = defaultImage;
-    }
-    else{
-      srcImage = srcImage.orignal;
-      console.log(srcImage)
-    }
+    const srcImage = show.image.original;
+
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
